5f887b6f2d621e1f9475e25afb713494db05475a
IT21191688 <IT21191688@my.sliit.lk>
2023-08-04 22:07:08 +0530

    create module,components,routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { LoginComponent } from './modules/security/components/login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+ { path: '', component: LoginComponent },
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  {path:'**', component:NotFoundComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
